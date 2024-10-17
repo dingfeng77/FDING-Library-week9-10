7c908194c99a37db8f51b43d5a535b54ca98863a
fdin0008 <fdin0008@student.monash.edu>
2024-10-17 20:50:54 +1100

    fix weather search

--- src/views/WeatherView.vue
+++ src/views/WeatherView.vue
@@ -46,46 +46,27 @@ export default {
     return {
       city: "",
       weatherData: null,
-      hourlyForecast: [],
-      dailyForecast: [],
     };
   },
   computed: {
-    //There are multiple way to obtain the data in Celsius format.
-    //Calculation by yourself like below after data is retireved or via API parameters
-    
-    //Example of adding additional units requirement, if you choose this, remember to change section 3.1
-    //https://api.openweathermap.org/data/2.5/weather?lat=XXX&lon=-XXX.15&appid={API key}&units=metric
     temperature() {
-      return this.weatherData
-        ? Math.floor(this.weatherData.main.temp - 273)
-        : null;
+      return this.weatherData ? Math.floor(this.weatherData.main.temp) : null;
     },
-    //Get the current weather icon using the API link
     iconUrl() {
       return this.weatherData
-        ? `https://api.openweathermap.org/img/w/${this.weatherData.weather[0].icon}.png`
+        ? `https://openweathermap.org/img/w/${this.weatherData.weather[0].icon}.png`
         : null;
     },
   },
-  //There are two steps involved in this, 
-  //step 1: identify current location 
-  //step 2: after identify, get the weather data straight based on the current location.
   mounted() {
     this.fetchCurrentLocationWeather();
   },
   methods: {
-    //Async in a easy to understand way means the method will run in backend thread, 
-    //And it won't occupy the main thread, so the user experience is still smooth
     async fetchCurrentLocationWeather() {
-      //The navigator.geolocation object is part of the Web API provided by modern web browsers
-      //Please note this function is not belongs to Vue or openweather.
       if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(async (position) => {
           const { latitude, longitude } = position.coords;
-          //API link to obtain the current weather based on the current location browser identified 
-          const url = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apikey}`;
-          //await means wait for the fetchWeatherData method to complete before proceeding
+          const url = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apikey}&units=metric`;
           await this.fetchWeatherData(url);
         });
       }
@@ -93,12 +74,20 @@ export default {
     async fetchWeatherData(url) {
       try {
         const response = await axios.get(url);
-        //Returned data from API is stored as JSON file in weatherData
         this.weatherData = response.data;
       } catch (error) {
         console.error("Error fetching weather data:", error);
       }
+    },
+    async searchByCity() {
+      if (this.city) {
+        const url = `https://api.openweathermap.org/data/2.5/weather?q=${this.city}&appid=${apikey}&units=metric`;
+        await this.fetchWeatherData(url);
+      } else {
+        console.error("Please enter a city name");
+      }
     }
-},
-}
+  }
+};
 </script>
+
