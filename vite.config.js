a11acad7d4110e0d8d99b337a535d9763c670baf
fdin0008 <fdin0008@student.monash.edu>
2024-10-17 19:57:32 +1100

    fix problems

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
-import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -13,7 +12,7 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src')
+      '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   }
 })
